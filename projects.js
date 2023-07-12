3335ed5af939a3ca6c493dfa7c4a433dedbd46c6
Mateusz <m.wesolowski.92@gmail.com>
2023-07-12 21:33:56 +0200

    change links to project 1

--- projects.js
+++ projects.js
@@ -1,26 +1,26 @@
 const projects = [
-    {
-        id: 0,
-        image: "/images/projects/Project-1.PNG",
-        linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-        linkGit: 'https://github.com/MateuszWesolowsky/Media-SHop'
-    },
-    {
-        id: 1,
-        image: "/images/projects/Project-2.PNG",
-        linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-        linkGit: 'https://github.com/MateuszWesolowsky/Media-SHop'
-    },
-    {
-        id: 2,
-        image: "/images/projects/Project-3.PNG",
-        linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-        linkGit: 'https://github.com/MateuszWesolowsky/Portfolio'
-    },
-    {
-        id: 3,
-        image: "/images/projects/Project-4.jpg",
-        linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-        linkGit: 'https://github.com/MateuszWesolowsky/Media-SHop'
-    }
-]
+	{
+		id: 0,
+		image: "/images/projects/Project-1.PNG",
+		linkLive: "/",
+		linkGit: "https://github.com/MateuszWesolowsky/LeagueOfLegendsCards-App",
+	},
+	{
+		id: 1,
+		image: "/images/projects/Project-2.PNG",
+		linkLive: "https://main--verdant-sprinkles-2393b4.netlify.app/",
+		linkGit: "https://github.com/MateuszWesolowsky/Media-SHop",
+	},
+	// {
+	//     id: 2,
+	//     image: "/images/projects/Project-3.PNG",
+	//     linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
+	//     linkGit: 'https://github.com/MateuszWesolowsky/Portfolio'
+	// },
+	// {
+	//     id: 3,
+	//     image: "/images/projects/Project-4.jpg",
+	//     linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
+	//     linkGit: 'https://github.com/MateuszWesolowsky/Media-SHop'
+	// }
+];
