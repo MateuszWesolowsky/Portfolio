287efe17f36a64e657aafba69857c2c92ae6bf7a
Mateusz <m.wesolowski.92@gmail.com>
2023-09-06 00:02:12 +0200

    add two projects

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projects = [
 	{
 		id: 0,
 		image: "/images/projects/Project-1.PNG",
-		linkLive: "/",
+		linkLive: "https://league-champions-app.netlify.app/",
 		linkGit: "https://github.com/MateuszWesolowsky/LeagueOfLegendsCards-App",
 	},
 	{
@@ -11,16 +11,16 @@ const projects = [
 		linkLive: "https://main--verdant-sprinkles-2393b4.netlify.app/",
 		linkGit: "https://github.com/MateuszWesolowsky/Media-SHop",
 	},
-	// {
-	//     id: 2,
-	//     image: "/images/projects/Project-3.PNG",
-	//     linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-	//     linkGit: 'https://github.com/MateuszWesolowsky/Portfolio'
-	// },
-	// {
-	//     id: 3,
-	//     image: "/images/projects/Project-4.jpg",
-	//     linkLive: 'https://main--verdant-sprinkles-2393b4.netlify.app/',
-	//     linkGit: 'https://github.com/MateuszWesolowsky/Media-SHop'
-	// }
+	{
+	    id: 2,
+	    image: "/images/projects/Project-3.PNG",
+	    linkLive: 'https://travellist-app.netlify.app/',
+	    linkGit: 'https://github.com/MateuszWesolowsky/travel-list'
+	},
+	{
+	    id: 3,
+	    image: "/images/projects/Project-4.jpg",
+	    linkLive: '/',
+	    linkGit: 'https://github.com/MateuszWesolowsky/Movie-App'
+	}
 ];
